20c8c6ba2e0b534ab389ab0aba023319ccbb2905
Omkar Khodwe <bim2017007@iiita.ac.in>
2020-10-30 14:44:20 +0530

    kosaraju

--- kosaraju.cpp
+++ kosaraju.cpp
@@ -2,12 +2,39 @@
 // Created by omkarkh1 on 30/10/20.
 //
 
+/*
+ Input:
+7 8
+2 1
+1 0
+0 2
+0 3
+3 5
+5 6
+6 3
+3 4
+ */
 #include <bits/stdc++.h>
 using namespace std;
 
 void dfs(vector<int> graph[],int node,bool* visited,vector<int> &stack){
     visited[node] = true;
+    for(auto nbr : graph[node]){
+        if(!visited[nbr]){
+            dfs(graph,nbr,visited,stack);
+        }
+    }
+    stack.push_back(node);
+}
 
+void dfs2(vector<int> graph[],int node,bool* visited,vector<int> &result){
+    visited[node] = true;
+    result.push_back(node);
+    for(auto nbr : graph[node]){
+        if(!visited[nbr]){
+            dfs2(graph,nbr,visited,result);
+        }
+    }
 }
 
 void solve(vector<int> graph[],vector<int>rgraph[],int n){
@@ -18,6 +45,22 @@ void solve(vector<int> graph[],vector<int>rgraph[],int n){
             dfs(graph,i,visited,stack);
         }
     }
+
+    memset(visited,false,n);
+    reverse(stack.begin(),stack.end());
+    vector<int> result;
+    for(auto node : stack){
+        if(!visited[node]){
+            result.clear();
+            dfs2(rgraph,node,visited,result);
+            //cout<<result.size()<<endl;
+            for(auto x :  result){
+                cout << x << " ";
+            }
+            cout<<endl;
+        }
+    }
+
 }
 int main(){
     int n;cin >> n;
@@ -30,6 +73,6 @@ int main(){
         graph[x].push_back(y);
         rgraph[y].push_back(x);
     }
-
+    solve(graph,rgraph,n);
     return 0;
 }
